463fc8f1399b07a429cc831706b0eb064e99d3bd
walker <walker>
2000-11-15 16:57:22 +0000

    made tree into combatant!

--- sources/jcavern/Tree.java
+++ sources/jcavern/Tree.java
@@ -8,10 +8,28 @@
 
 package jcavern;
 
-public class Tree extends Thing
+public class Tree extends Combatant
 {
-	public Tree() { super("Tree"); }
+	public Tree()
+	{
+		super("Tree", 0);
+	}
+	
+	public Object clone()
+	{
+		return new Tree();
+	}
 	
 	public String getAppearance() { return "T"; }
+	
+	public void gainExperience(Combatant opponent) { }
+	
+	public void decrementRangedAttackCount() { }
+	
+	public int computeDamage(Combatant opponent) { return 0; }
+	
+	public int computeRangedDamage(Combatant opponent) { return 0; }
+	
+	public int getWorth() { return 0; }
 }
 
