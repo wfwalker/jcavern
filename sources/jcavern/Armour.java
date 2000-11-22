f4b57b4b8860af8f21f03b01ea5ad791d08d3cf4
walker <walker>
2000-11-22 18:52:26 +0000

    added accessors, cloning support

--- sources/jcavern/Armour.java
+++ sources/jcavern/Armour.java
@@ -11,5 +11,26 @@ package jcavern;
 public class Armour extends Treasure
 {
 	private int		mPoints;
+	
+	public Armour(String name, int points)
+	{
+		super(name);
+		mPoints = points;
+	}
+	
+	public String toString()
+	{
+		return getName() + " +" + mPoints;
+	}
+	
+	public Object clone()
+	{
+		return new Armour(new String(getName()), mPoints);
+	}
+	
+	public int getPoints()
+	{
+		return mPoints;
+	}
 }
 
