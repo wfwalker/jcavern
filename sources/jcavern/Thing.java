4b9f0e2ad90f93d5448a4dff9b55677792150e22
walker <walker>
2000-12-09 02:36:28 +0000

    now generating textual apperance automatically from name; now retrieving image only on demand (helps with unit testing)

--- sources/jcavern/Thing.java
+++ sources/jcavern/Thing.java
@@ -15,14 +15,23 @@ public abstract class Thing extends Observable implements Cloneable
 	private String		mName;
 	
 	/** * The name of the image to use for this thing. */
+	private Image		mImage;
+
+	/** * The name of the image to use for this thing. */
 	private String		mImageName;
 
+	/** * The textual appearance of the Thing (usually two letters). */
+	private String		mTextSymbol;
+	
 	/**
 	 * Creates a new thing for the given name.
 	 */
 	public Thing(String aName)
 	{
 		mName = aName;
+		mTextSymbol = aName.substring(0, 2);
+		mImageName = "none";
+		mImage = null;
 	}
 	
 	/**
@@ -32,6 +41,17 @@ public abstract class Thing extends Observable implements Cloneable
 	{
 		mName = aName;
 		mImageName = imageName;
+		mImage = null;
+	}
+	
+	public Image getImage()
+	{
+		if ((mImage == null) && (JCavernApplet.current() != null))
+		{
+			mImage = JCavernApplet.current().getBoardImage(mImageName);
+		}
+		
+		return mImage;
 	}
 	
 	public String getImageName()
@@ -61,24 +81,23 @@ public abstract class Thing extends Observable implements Cloneable
 	
 	public void paint(Graphics g, int plotX, int plotY)
 	{
-		if (mImageName == null)
+		if (getImage() == null)
 		{
-			g.drawString(getAppearance(), plotX, plotY);
+			g.drawString(getTextSymbol(), plotX, plotY);
 		}
 		else
 		{
-			Image theImage = JCavernApplet.current().getBoardImage(mImageName);
-		
+			Image theImage = getImage();
 			g.drawImage(theImage, plotX - theImage.getWidth(null) / 2, plotY - theImage.getHeight(null) / 2, null);
 		}
 	}
 	
 	/**
-	 * Returns the one character string that is the appearance of this thing.
+	 * Returns the two character string that is the textual apperance of this thing.
 	 */
-	public String getAppearance()
+	public String getTextSymbol()
 	{
-		return "?";
+		return mTextSymbol;
 	}
 }
 
