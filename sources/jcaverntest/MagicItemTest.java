7a8a3332ef1326092b4034cb49723dfcbda9ee78
walker <walker>
2001-01-04 17:03:53 +0000

    added javadoc new constructor API

--- sources/jcaverntest/MagicItemTest.java
+++ sources/jcaverntest/MagicItemTest.java
@@ -7,7 +7,7 @@ import java.awt.Rectangle;
 import jcavern.*;
 
 /**
- * Tests functionality of Location.
+ * Tests functionality of MagicItem.
  *
  * @author	Bill Walker
  * @version	$Id$
@@ -22,6 +22,8 @@ public class MagicItemTest extends TestCase
 	
 	/**
 	 * Builds a world and a player, puts the player in the world.
+	 *
+	 * @exception	ThingCollisionException		placed two things in the same place
 	 */
 	public void setUp() throws ThingCollisionException
 	{
@@ -43,9 +45,15 @@ public class MagicItemTest extends TestCase
 		super(name);
 	}
 	
+	/**
+	 * Tests whether the reveal invisibility magic item works.
+	 *
+	 * @exception	ThingCollisionException		placed two things in the same place
+	 * @exception	JCavernInternalError		some internal problem
+	 */
 	public void testRevealInvisibility() throws ThingCollisionException, JCavernInternalError
 	{
-		Monster		testMonster = new Monster("invisio", "monster", 1, 1, true);
+		Monster		testMonster = new Monster("invisio", "monster", "hit", "killed", 1, 1, true);
 		MagicItem	testItem = new MagicItem("revealio", 1);
 		mWorld.place(new Location(6, 6), testMonster);
 		
