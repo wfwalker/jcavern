9563b9e952e389647d24eaaee8a118758fe8f2d6
walker <walker>
2001-01-15 16:43:40 +0000

    moved list of images to JCavernApplet, now using the same list for creating prototypes and loading images

--- sources/jcavern/MonsterFactory.java
+++ sources/jcavern/MonsterFactory.java
@@ -21,8 +21,6 @@ import java.io.*;
  */
 public class MonsterFactory
 {
-	private static final String		gImageNames[] = { "monster", "demon", "wraith", "rajah", "eyeball", "darklord", "chavin", "wahoo", "gobbler", "jackolantern", "snake" };
-	
 	/** * A list of Monsters stored in the order they were read from monsters.dat. */
 	public static Vector			gMonsters;
 	
@@ -114,7 +112,7 @@ public class MonsterFactory
 				}
 				else
 				{
-					imageName = gImageNames[(imageNameIndex++) % gImageNames.length];
+					imageName = JCavernApplet.MonsterImageNames[(imageNameIndex++) % JCavernApplet.MonsterImageNames.length];
 				}
 
 				gMonsters.addElement(
