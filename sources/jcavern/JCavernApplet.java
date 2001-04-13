3e32c2c2d6b65752bf62c06e84fbc969c744b93e
walker <walker>
2001-04-13 15:48:48 +0000

    removed static global player tracking

--- sources/jcavern/JCavernApplet.java
+++ sources/jcavern/JCavernApplet.java
@@ -26,67 +26,35 @@ import java.applet.*;
  */
 public class JCavernApplet extends Applet
 {
+	/** * The official PLATO orange color, more or less. */
 	public static final Color		CavernOrange = new Color(0xFF, 0x66, 0x00);
 	
+	/** * Names of image files containing pictures of monsters. */
 	public static final String		MonsterImageNames[] = {
 		"crab", "snail", "jubjub", "blob", "hoplite", "guy", "monster", "trex",
 		"demon", "wraith", "rajah", "grump", "larry", "eyeball", "ugly", "darklord", "chavin",
 		"wahoo", "gobbler", "scary", "jackolantern", "hippo", "alien", "forkman", "snake" };
-		
+	
+	/** * Names of image files containing pictures of non-monsters. */
 	public static final String		OtherImageNames[] = {
 		"player", "chest", "tree", "tree2", "castle", "splat" };
 
-	/** * The current player. */
-	private Player					mCurrentPlayer;
-	
 	/** * A table of messages */
 	private Hashtable				mImages;
 
 	/** * A MediaTracker to make sure the images get loaded. */
 	private MediaTracker			mTracker;
 
-	/** * The current applet */
-	private static JCavernApplet	gApplet;
-
-	private static JCavernApplet current()
+	/**
+	 * Displays the home card.
+	 *
+	 * @param		inPlayer				the Player currently in use
+	 * @exception	JCavernInternalError	trouble displaying the card
+	 */
+	public void displayHomeCard(Player inPlayer) throws JCavernInternalError
 	{
-		return gApplet;
-	}
-	
-	public static void displayHomeCard()
-	{
-		try
-		{
-			if (current() != null)
-			{
-				current().privateDisplayHomeCard();
-			}
-		}
-		catch (JCavernInternalError jcie)
-		{
-			System.out.println("can't display home card " + jcie);
-		}
-	}
-	
-	private void privateDisplayHomeCard() throws JCavernInternalError
-	{
-		IndexCard anIndex = new IndexCard(current(), mCurrentPlayer);
+		IndexCard anIndex = new IndexCard(this, inPlayer);
 		anIndex.show();
-	}
-	
-	public static void setPlayer(Player aPlayer)
-	{
-		if (current() != null)
-		{
-			current().privateSetPlayer(aPlayer);
-		}
-	}
-	
-	private void privateSetPlayer(Player aPlayer)
-	{
-		mCurrentPlayer = aPlayer;
-					
-		System.out.println("current player " + mCurrentPlayer + " is dead " + mCurrentPlayer.isDead());
 	}
 
 	/**
@@ -95,28 +63,24 @@ public class JCavernApplet extends Applet
 	public JCavernApplet()
 	{
 		mImages = new Hashtable();
-		gApplet = this;
+		//gApplet = this;
 		mTracker = new MediaTracker(this);
 	}
-	
-	public static Image getBoardImage(String aName) throws JCavernInternalError
-	{
-		if (current() != null)
-		{
-			return current().privateGetBoardImage(aName);
-		}
-		else
-		{
-			return null;
-		}
-	}
-	
-	private Image privateGetBoardImage(String aName) throws JCavernInternalError
+		
+	/**
+	 * Retrieves a board image.
+	 *
+	 * @param		aName					the name of the image to retrieve
+	 * @return								a non-null Image
+	 * @exception	JCavernInternalError	the image could not be retrieved
+	 */
+	public Image getBoardImage(String aName) throws JCavernInternalError
 	{
 		if (! mImages.containsKey(aName))
 		{
 			throw new JCavernInternalError("no such image " + aName + " in dictionary " + mImages);
 		}
+		
 		return (Image) mImages.get(aName);
 	}
 	
@@ -127,12 +91,22 @@ public class JCavernApplet extends Applet
 	{
 	}
 
-	// Get applet information
+	/**
+	 * Returns information about the applet.
+	 *
+	 * @return	a non-null String
+	 */
 	public String getAppletInfo() 
 	{
 		return "JCavern 0.0, Bill Walker";
 	}
 
+	/**
+	 * Loads data files from the web server.
+	 * These include monster prototypes, treasure prototypes, monster images, and other images.
+	 *
+	 * @exception	JCavernInternalError	could not load data
+	 */
 	private void loadDataFromServer() throws JCavernInternalError
 	{
 		try
@@ -150,6 +124,12 @@ public class JCavernApplet extends Applet
 		loadArrayOfImages(OtherImageNames);
 	}
 	
+	/**
+	 * Loads an array of images.
+	 *
+	 * @param		imageNames				an array of non-null Strings containing the names of image files.
+	 * @exception	JCavernInternalError	could not load data
+	 */
 	private void loadArrayOfImages(String[] imageNames) throws JCavernInternalError
 	{		
 		for (int index = 0; index < imageNames.length; index++)
