f0cc57a1f97a79e9d3c17fd2e7f78c5538889cad
walker <walker>
2001-04-13 15:34:06 +0000

    added javadoc comments can run with null image (used as a spacer in layouts)

--- sources/jcavern/ui/ImageCanvas.java
+++ sources/jcavern/ui/ImageCanvas.java
@@ -10,8 +10,26 @@ import jcavern.*;
  */
 public class ImageCanvas extends Canvas
 {
+	/** * The image to display. */
 	Image	mImage;
 	
+	/**
+	 * Creates a new, blank canvas with no image.
+	 *
+	 * @param	inWidth		the width of the canvas
+	 * @param	inHeight	the height of the canvas
+	 */
+	public ImageCanvas(int inWidth, int inHeight)
+	{
+		mImage = null;
+		setSize(inWidth, inHeight);
+	}
+	
+	/**
+	 * Creates a new canvas to show the given image.
+	 *
+	 * @param	anImage		a non-null Image to display.
+	 */
 	public ImageCanvas(Image anImage)
 	{
 		mImage = anImage;
@@ -19,8 +37,20 @@ public class ImageCanvas extends Canvas
 		setSize(mImage.getWidth(null), mImage.getHeight(null));	
 	}
 	
+	/**
+	 * Paints the image.
+	 *
+	 * @param	g	the non-null Graphics on which to paint.
+	 */
 	public void paint(Graphics g)
 	{
-		g.drawImage(mImage, 0, 0, null);
+		if (mImage != null)
+		{
+			g.drawImage(mImage, 0, 0, null);
+		}
+		else
+		{
+			// TODO: Erase the rect?
+		}
 	}
 }
